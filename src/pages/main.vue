4cf64a84338daf9f4d03127ad2b8f1c34b59c566
orangelckc <leockc812@gmail.com>
2025-03-28 20:58:42 +0100

    refactor: abstract live2d class and refactor hook logic

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,81 +1,28 @@
 <script setup lang="ts">
-import { LogicalSize } from '@tauri-apps/api/dpi'
 import { getCurrentWebviewWindow } from '@tauri-apps/api/webviewWindow'
-import { Live2DModel } from 'pixi-live2d-display'
-import { Ticker } from 'pixi.js'
-import { onMounted, ref, watch } from 'vue'
+import { nextTick, onMounted, onUnmounted, watch } from 'vue'
+
 import { useDevice } from '../composables/useDevice'
 import { useModel } from '../composables/useModel'
-import { getCursorMonitor } from '../utils/monitor'
 
-Live2DModel.registerTicker(Ticker)
-
-const mode = ref('standard')
 const { pressedKeys, pressedMouses, mousePosition } = useDevice()
-const { model, setParameterValue } = useModel()
+const { handleLoadModel, handleDestroy, handleMouseClick, handleResized, handleMouseMove, handleKeyDown, background } = useModel()
 
-onMounted(() => {
+onMounted(async () => {
   window.addEventListener('resize', handleResized)
-})
-
-watch(model, (value) => {
-  if (!value) return
-
+  await nextTick()
+  await handleLoadModel()
   handleResized()
 })
 
-watch(pressedKeys, (value) => {
-  const hasArrowKey = value.some(key => key.endsWith('Arrow'))
-  const hasNonArrowKey = value.some(key => !key.endsWith('Arrow'))
-
-  setParameterValue('CatParamRightHandDown', hasArrowKey)
-  setParameterValue('CatParamLeftHandDown', hasNonArrowKey)
+onUnmounted(() => {
+  handleDestroy()
+  window.removeEventListener('resize', handleResized)
 })
 
-watch(pressedMouses, (value) => {
-  const isLeftDown = value.includes('Left')
-  const isRightDown = value.includes('Right')
-
-  setParameterValue('ParamMouseLeftDown', isLeftDown)
-  setParameterValue('ParamMouseRightDown', isRightDown)
-})
-
-watch(mousePosition, async () => {
-  if (!model.value) return
-
-  const monitor = await getCursorMonitor()
-
-  if (!monitor) return
-
-  const { size, cursorX, cursorY } = monitor
-  const { width, height } = size
-
-  const xRatio = cursorX / width
-  const yRatio = cursorY / height
-
-  const x = (xRatio * 60) - 30
-  const y = (yRatio * 60) - 30
-
-  setParameterValue('ParamMouseX', -x)
-  setParameterValue('ParamMouseY', -y)
-  setParameterValue('ParamAngleX', x)
-  setParameterValue('ParamAngleY', -y)
-})
-
-async function handleResized() {
-  if (!model.value) return
-
-  const { innerWidth } = window
-
-  await getCurrentWebviewWindow().setSize(
-    new LogicalSize({
-      width: innerWidth,
-      height: innerWidth * (354 / 612),
-    }),
-  )
-
-  model.value.scale.set(innerWidth / 612)
-}
+watch(pressedKeys, handleKeyDown)
+watch(mousePosition, handleMouseMove)
+watch(pressedMouses, handleMouseClick)
 
 function handleMouseDown() {
   const appWindow = getCurrentWebviewWindow()
@@ -89,7 +36,7 @@ function handleMouseDown() {
     class="relative children:(absolute h-screen w-screen)"
     @mousedown="handleMouseDown"
   >
-    <img :src="`/images/backgrounds/${mode}.png`">
+    <img :src="background">
 
     <canvas id="live2dCanvas" />
 
