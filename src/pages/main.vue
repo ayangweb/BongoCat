dd3412abb97843e872181bff8612a74399788b43
orangelckc <leockc812@gmail.com>
2025-03-28 21:30:50 +0100

    feat: add overlap when resizing window for better performance

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,23 +1,48 @@
 <script setup lang="ts">
 import { getCurrentWebviewWindow } from '@tauri-apps/api/webviewWindow'
-import { nextTick, onMounted, onUnmounted, watch } from 'vue'
+import { nextTick, onMounted, onUnmounted, ref, watch } from 'vue'
 
 import { useDevice } from '../composables/useDevice'
 import { useModel } from '../composables/useModel'
 
 const { pressedKeys, pressedMouses, mousePosition } = useDevice()
-const { handleLoadModel, handleDestroy, handleMouseClick, handleResized, handleMouseMove, handleKeyDown, background } = useModel()
+const { handleLoadModel, handleDestroy, handleMouseClick, handleResized, handleMouseMove, handleKeyDown, handleSetMode, mode, background } = useModel()
+
+const isOverLap = ref(false)
+let resizeTimer: NodeJS.Timeout | null = null
+
+async function handleSwitch() {
+  const newMode = mode.value === 'STANDARD' ? 'KEYBOARD' : 'STANDARD'
+  await handleSetMode(newMode)
+  handleResized()
+}
+
+function handleResize() {
+  isOverLap.value = true
+
+  if (resizeTimer) clearTimeout(resizeTimer)
+
+  resizeTimer = setTimeout(async () => {
+    await handleResized()
+    isOverLap.value = false
+  }, 100)
+}
 
 onMounted(async () => {
-  window.addEventListener('resize', handleResized)
   await nextTick()
   await handleLoadModel()
   handleResized()
+
+  window.addEventListener('resize', handleResize)
 })
 
 onUnmounted(() => {
   handleDestroy()
-  window.removeEventListener('resize', handleResized)
+
+  window.removeEventListener('resize', handleResize)
+
+  if (resizeTimer)
+    clearTimeout(resizeTimer)
 })
 
 watch(pressedKeys, handleKeyDown)
@@ -36,6 +61,16 @@ function handleMouseDown() {
     class="relative children:(absolute h-screen w-screen)"
     @mousedown="handleMouseDown"
   >
+    <div v-if="isOverLap" class="absolute inset-0 z-99 flex items-center justify-center bg-black">
+      <span class="text-center text-5xl text-white">
+        重绘中...
+      </span>
+    </div>
+
+    <button class="absolute left-0 top-0 z-9 rounded-full bg-sky text-center text-white h-8! w-20! hover:shadow-lg" @click.stop="handleSwitch">
+      切换模式
+    </button>
+
     <img :src="background">
 
     <canvas id="live2dCanvas" />
