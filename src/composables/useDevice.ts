9fb5aec771e1d65c5a541f8d5485c533fe3325ea
ayang <473033518@qq.com>
2025-03-29 11:00:36 +0800

    refactor: replacement of image resources and optimization of keyboard event handling

--- src/composables/useDevice.ts
+++ src/composables/useDevice.ts
@@ -1,6 +1,7 @@
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 
 import { LISTEN_KEY } from '../constants'
+import { useModelStore } from '../stores/model'
 
 import { useTauriListen } from './useTauriListen'
 
@@ -28,17 +29,41 @@ interface KeyboardEvent {
 
 type DeviceEvent = MouseButtonEvent | MouseMoveEvent | KeyboardEvent
 
+const keyFiles = import.meta.glob('../assets/images/keys/*.png', { eager: true })
+
 export function useDevice() {
   const pressedMouses = ref<MouseButtonValue[]>([])
   const mousePosition = ref<MouseMoveValue | undefined>()
   const pressedKeys = ref<string[]>([])
+  const modelStore = useModelStore()
 
-  const handlePress = <T>(array: T[], value: T) => {
+  const supportKeys = computed(() => {
+    return Object.keys(keyFiles).map((path) => {
+      return path.split('/').pop()?.replace('.png', '')
+    }).filter(Boolean)
+  })
+
+  const handlePress = <T>(array: T[], value?: T) => {
+    if (!value) return array
+
     return [...new Set([...array, value])]
   }
 
-  const handleRelease = <T>(array: T[], value: T) => {
+  const handleRelease = <T>(array: T[], value?: T) => {
+    if (!value) return array
+
     return array.filter(item => item !== value)
+  }
+
+  const normalizeKeyValue = (key: string) => {
+    key = key.replace(/(Left|Right|Gr)$/, '').replace(/F(\d+)/, 'Fn')
+
+    const isInvalidArrowKey = key.endsWith('Arrow') && modelStore.mode !== 'KEYBOARD'
+    const isUnsupportedKey = !supportKeys.value.includes(key)
+
+    if (isInvalidArrowKey || isUnsupportedKey) return
+
+    return key
   }
 
   useTauriListen<DeviceEvent>(LISTEN_KEY.DEVICE_CHANGED, ({ payload }) => {
@@ -55,17 +80,17 @@ export function useDevice() {
         mousePosition.value = value
         break
       case 'KeyboardPress':
-        pressedKeys.value = handlePress(pressedKeys.value, value)
+        pressedKeys.value = handlePress(pressedKeys.value, normalizeKeyValue(value))
         break
       case 'KeyboardRelease':
-        pressedKeys.value = handleRelease(pressedKeys.value, value)
+        pressedKeys.value = handleRelease(pressedKeys.value, normalizeKeyValue(value))
         break
     }
   })
 
   return {
     pressedMouses,
-    pressedKeys,
     mousePosition,
+    pressedKeys,
   }
 }
