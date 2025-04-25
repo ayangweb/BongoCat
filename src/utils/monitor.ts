fad587a5615c33e71fe540ef168a2ec1b4d097bc
王子凌 <sling1994@live.com>
2025-04-25 11:04:48 +0800

    chore: enhance the utilities of `get cursor monitor`

--- src/utils/monitor.ts
+++ src/utils/monitor.ts
@@ -1,17 +1,27 @@
 import {
   availableMonitors,
   cursorPosition,
+  primaryMonitor,
 } from '@tauri-apps/api/window'
 
+/**
+ * 获取当前鼠标所在的显示器
+ */
 export async function getCursorMonitor() {
+  const primary = await primaryMonitor()
+
   const monitors = await availableMonitors()
 
-  if (!monitors.length) return
+  if (!primary || !monitors.length) return
 
-  const { x, y } = await cursorPosition()
+  const mousePosition = await cursorPosition()
+  const { x, y } = mousePosition.toLogical(primary.scaleFactor)
 
   const monitor = monitors.find((monitor) => {
-    const { position, size } = monitor
+    const { scaleFactor } = monitor
+
+    const position = monitor.position.toLogical(scaleFactor)
+    const size = monitor.size.toLogical(scaleFactor)
 
     const inX = x >= position.x && x <= position.x + size.width
     const inY = y >= position.y && y <= position.y + size.height
@@ -21,9 +31,13 @@ export async function getCursorMonitor() {
 
   if (!monitor) return
 
+  const { scaleFactor, size, position } = monitor
+
   return {
     ...monitor,
     cursorX: x,
     cursorY: y,
+    size: size.toLogical(scaleFactor),
+    position: position.toLogical(scaleFactor),
   }
 }
