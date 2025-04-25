f7361f58cca403a730fc03d0a11e0b52d131940d
王子凌 <sling1994@live.com>
2025-04-25 11:03:53 +0800

    Revert "fix: fix the wrong cursor position between multiple monitors"
    This reverts commit 609deb4e320bf4f877a99dc69a35d571f93f754d.

--- src/utils/monitor.ts
+++ src/utils/monitor.ts
@@ -23,7 +23,7 @@ export async function getCursorMonitor() {
 
   return {
     ...monitor,
-    cursorX: x - monitor.position.x,
-    cursorY: y - monitor.position.y,
+    cursorX: x,
+    cursorY: y,
   }
 }
