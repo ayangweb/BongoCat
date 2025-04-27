5df11cc84b87abeb7fe826c0fdc56d0b1f86c366
yyz <1104441592@qq.com>
2025-04-27 16:55:19 +0800

    feat: 增加开机自启功能

--- src/stores/general.ts
+++ src/stores/general.ts
@@ -3,8 +3,10 @@ import { ref } from 'vue'
 
 export const useGeneralStore = defineStore('general', () => {
   const autoCheckUpdate = ref(false)
+  const autoStart = ref(false)
 
   return {
     autoCheckUpdate,
+    autoStart,
   }
 })
