0b5408d44b71b1003267602d5d0bade63304cedd
ayang <473033518@qq.com>
2025-07-19 21:54:19 +0800

    feat: 支持导入游戏手柄模型

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -3,9 +3,9 @@ import { sep } from '@tauri-apps/api/path'
 export function join(...paths: string[]) {
   const joinPaths = paths.map((path, index) => {
     if (index === 0) {
-      return path.replace(new RegExp(`${sep()}$`), '')
+      return path.replace(new RegExp(`${sep()}+$`), '')
     } else {
-      return path.replace(new RegExp(`^${sep()}|${sep()}$`, 'g'), '')
+      return path.replace(new RegExp(`^${sep()}+|${sep()}+$`, 'g'), '')
     }
   })
 
