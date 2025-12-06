22c983ea5c795734b9561712d317805da764cb5f
SuellenMiranda <suellen.org@gmail.com>
2025-12-06 16:24:08 -0300

    feat: add Portuguese (pt-BR) language support and locale integration

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -4,11 +4,14 @@ import type { Locale as AntdLocale } from 'ant-design-vue/es/locale'
 import antdEnUS from 'ant-design-vue/locale/en_US'
 import antdViVN from 'ant-design-vue/locale/vi_VN'
 import antdZhCN from 'ant-design-vue/locale/zh_CN'
+// @ts-expect-error - pt_BR pode não existir em todas as versões do ant-design-vue
+import antdPtBR from 'ant-design-vue/locale/pt_BR'
 import { createI18n } from 'vue-i18n'
 
 import enUS from './en-US.json'
 import viVN from './vi-VN.json'
 import zhCN from './zh-CN.json'
+import ptBR from './pt-BR.json'
 
 import { LANGUAGE } from '@/constants'
 
@@ -20,6 +23,7 @@ export const i18n = createI18n({
     [LANGUAGE.ZH_CN]: zhCN,
     [LANGUAGE.EN_US]: enUS,
     [LANGUAGE.VI_VN]: viVN,
+    [LANGUAGE.PT_BR]: ptBR,
   },
 })
 
@@ -28,6 +32,7 @@ export function getAntdLocale(language: Language = LANGUAGE.EN_US) {
     [LANGUAGE.ZH_CN]: antdZhCN,
     [LANGUAGE.EN_US]: antdEnUS,
     [LANGUAGE.VI_VN]: antdViVN,
+    [LANGUAGE.PT_BR]: antdPtBR ?? antdEnUS, // Fallback para en_US se pt_BR não existir
   }
 
   return antdLanguage[language]
